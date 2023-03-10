86ff7dca6b5a44a0703919a06ca230a18101bacc
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-10 23:53:39 +0100

    updates

--- main.c
+++ main.c
@@ -5,19 +5,19 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc > 2) {
-            printf("Too many arguments!\n");
-            return 1;
-    }
-    else if (argc < 2) {
-            printf("Argument needed!\n");
-            return 1;
-    }
+        if (argc > 2) {
+                printf("Too many arguments!\n");
+                return 1;
+        }
+        else if (argc < 2) {
+                printf("Argument needed!\n");
+                return 1;
+        }
 
-    if(calculation(argv) == 1) {
-            printf("Error encountered while parsing your argument. Please make sure it's in this format: Variable, Symbol, Variable\n");
-            return 1;
-    }
-    else
-            return 0; // Success!
+        if(calculation(argv) == 1) {
+                printf("Error encountered while parsing your argument. Please make sure it's in this format: Variable, Symbol, Variable\n");
+                return 1;
+        }
+        else
+                return 0; // Success!
 }
